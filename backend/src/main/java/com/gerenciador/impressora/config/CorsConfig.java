8d2f4057b0590d4d342c0c4c7f13737f66f735c1
frogzinkkj <andre.liborio2005@gmail.com>
2026-05-28 10:30:12 -0300

    fix: fixed front ip in coors,app.prop and front env.ts

--- backend/src/main/java/com/gerenciador/impressora/config/CorsConfig.java
+++ backend/src/main/java/com/gerenciador/impressora/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.gerenciador.impressora.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,10 +12,14 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    
+    @Value("${app.cors.allowed-origins}")
+    private String frontendUrl;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(frontendUrl));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
